d50bd692651004d5ab50c9492b1d2f645315fa53
Desbog7 <fabianra999@gmail.com>
2019-01-10 16:57:19 -0500

    ajuste lista

--- mvc/mono/views/usuarios/mostrar-todos.php
+++ mvc/mono/views/usuarios/mostrar-todos.php
@@ -5,21 +5,18 @@
 				<h1 class="display-4">Listado de usuarios</h1>
 				<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 			</div>
-
-			<div class="card" style="width: 18rem;">
-			<img class="card-img-top" src=".../100px180/" alt="Card image cap">
-			<div class="card-body">
-				<h5 class="card-title">Card title</h5>
-				<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				<a href="#" class="btn btn-primary">Go somewhere</a>
-			</div>
-			</div>
-
 		</div>
 	</div>
+	<div class="row">
+		<?php while ($usuario = $usuarios->fetch_object()) : ?>
+		<div class="col-12 col-sm-4 mt-2">
+				<div class="card" style="width: 18rem;">
+				<div class="card-body">
+					<h5 class="card-title"><?= $usuario->nombre ?></h5>
+					<p class="card-text"><?= $usuario->nombre ?> | <?= $usuario->apellidos ?> | <?= $usuario->fecha ?>.</p>
+				</div>
+				</div>
+		</div>
+		<?php endwhile; ?>		
+	</div>
 </div>
-
-<h1></h1>
-<?php while ($usuario = $usuarios->fetch_object()) : ?>
-	<?= $usuario->nombre ?> - <?= $usuario->fecha ?> <br/>
-<?php endwhile; ?>
